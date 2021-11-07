5450670b3fc2de4e5901923707b85df38e4184ae
Luigi Renda <Luigi.Renda@nttdata.com>
2021-11-07 22:58:26 +0100

    fix dynamic label

--- blocks/01-dynamic-label/src/index.js
+++ blocks/01-dynamic-label/src/index.js
@@ -2,7 +2,6 @@
  * WordPress dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
-import { Component } from 'react';
 
 import json from '../block';
 
@@ -10,31 +9,22 @@ const { name, ...settings } = json;
 
 registerBlockType(name, {
     settings,
-    edit: class extends Component  {
-        constructor(props) {
-            super(props);
-            this.props = props;
-            this.state = {value: ''};
-        }
-
-        handleChange = (event) => {
-            this.setState({value: event.target.value});
-            this.props.attributes.text = event.target.value;
+    edit: (props) => {
+        const handleChange = ( event ) => {
+            props.setAttributes( { text: event.target.value } );
         };
 
-        render() {
-            return (
-                <>
-                    <label>Type text:</label>
-                    <input type="text" value={this.props.attributes.text} onChange={this.handleChange} />
-                </>
-            )
-        }
+        return (
+            <>
+                <label>Type text:</label>
+                <input type="text" value={props.attributes.text} onChange={handleChange} />
+            </>
+        )
     },
     save: (props) => {
         return (
             <>
-                <div><p>Text is: "{props.attributes.text}"</p></div>
+                <div><p>Text is: {props.attributes.text}</p></div>
             </>
         )
     }
